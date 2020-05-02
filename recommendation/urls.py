b524e755a010e8792c930626455d10bf0060ccb7
blackmamba1997 <36701947+blackmamba1997@users.noreply.github.com>
2020-05-02 11:19:55 +0530

    update urls.py

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('test/<int:text>/', views.home, name='home'),
+    path('test/<int:text>/<keyword>/', views.home, name='home'),
     path('about/',views.about, name='about')
 ]
